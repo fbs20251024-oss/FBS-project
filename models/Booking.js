8308cddd22f1dafc89fcdc7e480cc7ad072011f0
fbs <fbs20251024@gmail.com>
2025-10-30 11:21:35 +0800

    Refactor Booking.js to use ES6 import syntax

--- models/Booking.js
+++ models/Booking.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const bookSchema = new mongoose.Schema(
   {
@@ -10,5 +10,6 @@ const bookSchema = new mongoose.Schema(
   },
   { timestamps: true }
 )
+
 
 module.exports = mongoose.model("Booking", bookSchema);
