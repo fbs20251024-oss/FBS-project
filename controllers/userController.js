6d605e81fed4c1cb3b308db39a3216743ee6ec6c
fbs <fbs20251024@gmail.com>
2025-10-31 15:13:48 +0800

    Add files via upload
    1) create API for updating User data
    2) create API for admin updating User data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 export const getUsers = async (req, res) => {
   try {
@@ -45,6 +46,91 @@ export const getProfile = async (req, res) => {
     res.status(200).json(user);
   } catch (error) {
     console.error("Error fetching profile:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+//API for Updating User Profile
+export const updateProfile = async (req, res) =>{
+  try {
+    const userId = req.user?.id; // 從JWT解碼結果取得user id
+    if (!userId){
+      return res.status(401).json({ success: false, msg: 'Unauthorized: No user logged in' });
+        };
+
+    const {username, email, password} = req.body;
+
+    if (!email && !password) { 
+            return res.status(400).json({
+                success:false, msg: "Please enter correct update information"
+            });
+        };      
+
+    //Const UpdateFields
+    const updateFields = {};
+    if(username) updateFields.username = username;
+    if(email) updateFields.email = email;
+    if(password) { //Handle Password
+      const hashedPassword = await bcrypt.hash(password, 10);
+      updateFields.password = hashedPassword;
+      };
+
+    const updateProfile = await User.findByIdAndUpdate(userId,{$set: updateFields},{new:true});
+
+    if (! updateProfile) {
+            return res.status(404).json({
+                success:false, msg: "Profile not found"
+                });
+        };
+    
+  } catch (error) {
+    console.error("Error updating profile:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+//API for Admin to Update User Profile (Password not included)
+export const adminUpdateProfile = async (req, res) =>{
+  try {
+    const adminRole = req.user?.role; // 從JWT解碼結果取得user role
+    if (!adminRole || adminRole !== 'admin'){
+      return res.status(401).json({ success: false, msg: 'Access denied' });
+        };
+
+    const {username, email, role} = req.body;
+    const userId = req.params.id;
+
+    if (!username && !email && !password) { 
+            return res.status(400).json({
+                success:false, msg: "Please enter correct update information"
+            });
+    };      
+
+    //Const UpdateFields
+    const updateFields = {};
+    if(username) updateFields.username = username;
+    if(email) updateFields.email = email;
+    if(role) {
+      if(!['user', 'admin'].includes(role)){
+        return res.status(400).json({ success: false, msg: 'Invalid role' });
+      }
+      updateFields.role = role;
+    };
+
+    if (Object.keys(updateFields).length === 0) {
+      return res.status(400).json({ success: false, msg: 'Nothing to update' });
+    }
+
+    const updateProfile = await User.findByIdAndUpdate(userId,{$set: updateFields},{new:true});
+
+    if (! updateProfile) {
+            return res.status(404).json({
+                success:false, msg: "Profile not found"
+                });
+        };
+    
+  } catch (error) {
+    console.error("Error updating profile:", error);
     return res.status(500).json({ message: "Server error" });
   }
 };
