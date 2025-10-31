fdfb12ed12090698c9c3618faeaf901e93fe60e7
fbs <fbs20251024@gmail.com>
2025-10-31 15:16:58 +0800

    create update routes
    1) router.put("update/me", verifyToken, updateProfile);
    2) router.put("update/:id", verifyToken, verifyRole("admin"), adminUpdateProfile);

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,16 @@ import {
   deleteUser,
   getProfile,
   getUsers,
+  updateProfile,
+  adminUpdateProfile
 } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/", verifyToken, verifyRole("admin"), getUsers);
 router.get("/me", verifyToken, getProfile);
-//router.get("update/me", verifyToken, updateProfile);
+router.put("update/me", verifyToken, updateProfile);
+router.put("update/:id", verifyToken,verifyRole("admin"), adminUpdateProfile);
 router.delete("/:id", verifyToken, verifyRole("admin"), deleteUser);
 
 export default router;
