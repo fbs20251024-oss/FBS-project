49bfb83c78f22b16c0a78fa524c80a1f32bef0df
fbs <fbs20251024@gmail.com>
2025-10-31 16:04:03 +0800

    added and change some functions

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,35 +1,33 @@
 import Booking from "../models/Booking.js";
 
 export const addBooking = async (req, res) => {
-    const { facilityId, userId, date, session } = req.body;
+    const { booking: { facilityId, userId, date, session } } = req.body;
 
     if (!facilityId || !userId || !date || !session) {
         return res.status(400).json({ message: "All fields are required" });
     }
 
     try {
-        const existingBooking = await Booking.findOne({ facilityId, date, session });
+        const existingBooking = await Booking.findOne({ facility: facilityId, date, session });
         if (existingBooking) {
             return res.status(400).json({ message: "Booking already exists" });
         }
 
         const booking = new Booking({
-            facilityId,
-            userId,
+            facility: facilityId,
+            user: userId,
             date,
             session
         });
         await booking.save();
+
+        const newBooking = await Booking.findOne(booking._id)
+            .populate("user", "username")
+            .populate("facility", "facilityName");
+
         res.status(201).json({
             message: "Booking created successfully",
-            booking: {
-                id: booking._id,
-                facilityId: booking.facilityId,
-                userId: booking.userId,
-                date: booking.date,
-                session: booking.session,
-                bookingStatus: booking.bookingStatus,
-            },
+            newBooking,
         });
     } catch (error) {
         console.error("Error creating booking", error);
@@ -37,22 +35,80 @@ export const addBooking = async (req, res) => {
     }
 };
 
-export const getBookings = async (req, res) => {
+export const getBooking = async (req, res) => {
+    if (req.path.includes("/user/") || req.path.includes("/facility/")) {
         try {
-        let bookings;
+            let query = {};
 
-        if (req.path.includes("/user/")) {
-            bookings = await Booking.find({ userId: req.params.id });
-        } else if (req.path.includes("/facility/")) {
-            bookings = await Booking.find({ facilityId: req.params.id });
+            if (req.path.includes("/user/")) {
+                query.user = req.params.id;
+            } else if (req.path.includes("/facility/")) {
+                query.facility = req.params.id;
+            }
+
+            const bookings = await Booking.find(query)
+                .populate("user", "username")
+                .populate("facility", "facilityName");
+
+            if (!bookings || bookings.length === 0) {
+                return res.status(404).json({ message: "Booking not found" });
+            }
+            const total = bookings.length;
+            res.status(200).json({ bookings, total });
+        } catch (error) {
+            console.error("Error fetching booking:", error);
+            return res.status(500).json({ message: "Server error" });
         }
-        if (!bookings || bookings.length === 0) {
+    } else {
+        try {
+            const booking = await Booking.findById(req.params.id)
+                .populate("user", "username")
+                .populate("facility", "facilityName");
+
+            if (!booking || booking.length === 0) {
+                return res.status(404).json({ message: "Booking not found" });
+            }
+            res.status(200).json({ booking });
+        } catch (error) {
+            console.error("Error fetching booking:", error);
+            return res.status(500).json({ message: "Server error" });
+        }
+    }
+};
+
+export const updateBooking = async (req, res) => {
+    const { booking: { date, session, bookingStatus } } = req.body;
+
+    try {
+        const booking = await Booking.findById(req.params.id)
+            .populate("user", "username")
+            .populate("facility", "facilityName");
+
+        if (!booking) {
             return res.status(404).json({ message: "Booking not found" });
         }
 
-        res.status(200).json(bookings);
+        const facilityId = booking.facility._id;
+
+        const existingBooking = await Booking.findOne({
+            facility: facilityId, date, session, _id: { $ne: booking._id }
+        });
+        if (existingBooking) {
+            return res.status(400).json({ message: "Booking already exists" });
+        }
+
+        if (date) booking.date = date;
+        if (session) booking.session = session;
+        if (bookingStatus) booking.bookingStatus = bookingStatus;
+        await booking.save();
+
+        res.status(200).json({
+            message: "Booking updated successfully",
+            booking,
+        });
     } catch (error) {
-        console.error("Error fetching booking:", error);
+        console.error("Error updating booking", error);
         return res.status(500).json({ message: "Server error" });
     }
 };
+
--- controllers/facilityController.js
+++ controllers/facilityController.js
@@ -1,7 +1,7 @@
 import Facility from "../models/Facility.js";
 
 export const createFacility = async (req, res) => {
-  const { facilityName, description } = req.body;
+  const { facility: { facilityName, description } } = req.body;
 
   if (!facilityName || !description) {
     return res.status(400).json({ message: "All fields are required" });
@@ -13,7 +13,7 @@ export const createFacility = async (req, res) => {
       return res.status(400).json({ message: "Facility already exists" });
     }
 
-    const facility= new Facility({
+    const facility = new Facility({
       facilityName,
       description,
     });
@@ -33,24 +33,59 @@ export const createFacility = async (req, res) => {
   }
 };
 
-export const getFacilities = async (req, res) => {
-  try {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 2;
-    const skip = (page - 1) * limit;
-    const total = await Facility.countDocuments();
-    const facilities = await Facility.find().skip(skip).limit(limit);
-    res.status(200).json({
-      facilities,
-      total,
-      totalPages: Math.ceil(total / limit),
-      currentPage: page,
-    });
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    return res.status(500).json({ message: "Server error" });
+export const getFacility = async (req, res) => {
+  if (req.params.id) {
+    try {
+      const facility = await Facility.findById(req.params.id);
+      if (!facility) {
+        return res.status(404).json({ message: "Facility not found" });
+      }
+      res.status(200).json({ facility });
+    } catch (error) {
+      console.error("Error fetching facility detail:", error);
+      return res.status(500).json({ message: "Server error" });
+    }
+  } else {
+    try {
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 2;
+      const skip = (page - 1) * limit;
+      const total = await Facility.countDocuments();
+      const facilities = await Facility.find().skip(skip).limit(limit);
+      res.status(200).json({
+        facilities,
+        total,
+        totalPages: Math.ceil(total / limit),
+        currentPage: page,
+      });
+    } catch (error) {
+      console.error("Error fetching facilities:", error);
+      return res.status(500).json({ message: "Server error" });
+    }
   }
 };
+
+export const updateFacility = async (req, res) => {
+  const { facility: { facilityName, description, facilityStatus } } = req.body;
+  try {
+    const facility = await Facility.findById(req.params.id);
+    if (!facility) {
+      return res.status(404).json({ message: "Facility not found" });
+    }
+    if (facilityName) facility.facilityName = facilityName;
+    if (description) facility.description = description;
+    if (facilityStatus) facility.facilityStatus = facilityStatus;
+    await facility.save();
+
+    res.status(200).json({
+      message: "Facility updated successfully",
+      facility,
+    });
+  } catch (error) {
+    console.error("Error updating facility:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+}
 
 export const deleteFacility = async (req, res) => {
   try {
@@ -62,19 +97,6 @@ export const deleteFacility = async (req, res) => {
     res.status(200).json({ message: "Facility deleted successfully" });
   } catch (error) {
     console.error("Error deleting facility:", error);
-    return res.status(500).json({ message: "Server error" });
-  }
-};
-
-export const getFacility = async (req, res) => {
-  try {
-    const facility = await Facility.findById(req.params.id);
-    if (!facility) {
-      return res.status(404).json({ message: "Facility not found" });
-    }
-    res.status(200).json(facility);
-  } catch (error) {
-    console.error("Error fetching facility detail:", error);
     return res.status(500).json({ message: "Server error" });
   }
 };
