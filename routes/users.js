7a5b8a0f64fb5ed10656a8c167d96a05945809ab
fbs <fbs20251024@gmail.com>
2025-11-03 12:13:19 +0800

    add admin to see users' profile

--- routes/users.js
+++ routes/users.js
@@ -11,8 +11,9 @@ const router = express.Router();
 
 router.get("/", verifyToken, verifyRole("admin"), getUsers);
 router.get("/me", verifyToken, getProfile);
+router.get("/:id", verifyToken, verifyRole("admin"), getProfile);
 router.put("/", verifyToken, updateProfile);
-router.put("/:id", verifyToken,verifyRole("admin"), updateProfile);
+router.put("/:id", verifyToken, verifyRole("admin"), updateProfile);
 router.delete("/:id", verifyToken, verifyRole("admin"), deleteUser);
 
 export default router;
